5baf82f6fa5f488d1451c806859235fd422abdc7
Kushal Pisavadia <kushal.pisavadia@digital.cabinet-office.gov.uk>
2014-08-07 17:04:47 +0100

    A default parameter can be set for keys that are added

--- lib/lrucache_compat/lru_cache.rb
+++ lib/lrucache_compat/lru_cache.rb
@@ -1,8 +1,9 @@
 module LRUCacheCompat
   class LRUCache
-    attr_reader :max_size
+    attr_reader :default, :max_size
 
     def initialize(opts = {})
+      @default = opts[:default]
       @max_size = Integer(opts[:max_size] || 100)
 
       raise "max_size must not be negative" if @max_size < 0
--- spec/lrucache_compat/lru_cache_spec.rb
+++ spec/lrucache_compat/lru_cache_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe LRUCacheCompat::LRUCache do
   describe "default initialisation" do
     subject { LRUCacheCompat::LRUCache }
+
     describe ":max_size" do
       it "has a default :max_size of 100" do
         expect(subject.new.max_size).to eq(100)
@@ -18,6 +19,17 @@ describe LRUCacheCompat::LRUCache do
 
       it "should raise an exception if :max_size parameter is converted to a negative integer" do
         expect { subject.new(:max_size => -1) }.to raise_exception
+      end
+    end
+
+    describe ":default" do
+      it "should have a :default parameter of nil" do
+        expect(subject.new.default).to be_nil
+      end
+
+      it "should accept a :default parameter" do
+        default = double(:default)
+        expect(subject.new(default: default).default).to eq(default)
       end
     end
   end
